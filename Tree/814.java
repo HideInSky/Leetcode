4a5e8bbdea6a38aec5cec341b75ad4f8f92fb3ae
Linnn <linyucheng112@qq.com>
2022-11-29 22:31:07 -0600

    TreeNode

---
+++ Tree/814.java
@@ -0,0 +1,27 @@
+package Tree;
+
+// 814. Binary Tree Pruning
+// 11-29
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root.val == 0){
+            pruneTree
+        }
+    }
+}
